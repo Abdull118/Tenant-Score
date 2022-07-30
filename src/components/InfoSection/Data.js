10153cfdc0ea48a65525a61b72c01eb6ab65b9d4
Ahmad Naimi <naimi.airsh@gmail.com>
2022-07-30 11:46:30 -0400

    A never before seen game changing real estate platform

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Features',
+    headline: 'Bundle vital scores with zero hidden fees',
+    description: 'Get access to our exclusive app that allows you to pull your credit report, background check, and income information for a Tenant Score. Avoid those hidden Real Estate fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require("../../images/svg-1.svg").default,
+    alt: 'house',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lighttextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'Access your updated score as many times as you need!',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require("../../images/svg-2.svg").default,
+    alt: 'Piggybanck',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+  
+  export const homeObjThree = {
+    id: 'signup',
+    lightBg: false,
+    lightText: true,
+    lighttextDesc: true,
+    topLine: 'Surf & Save',
+    headline: 'Creating an account is extremely easy',
+    description: 'Save tons by signing up today!',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require("../../images/svg-3.svg").default,
+    alt: 'car',
+    dark: true,
+    primary: true,
+    darkText: false
+  };
